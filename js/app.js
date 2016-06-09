a634bdc7707c3092f9d8fcfacd9b265729c0256e
petrero <petrero@gmail.com>
2016-06-09 19:58:00 +0300

    Detect current page with jQuery and make the coresponding top-bar menu distinct.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+$(document).ready(function(){
+  $('.index header ul li a:contains("Home")').parent().addClass('active');
+  $('.contact header ul li a:contains("Contact")').parent().addClass('active');
+  $('.gallery header ul li a:contains("Gallery")').parent().addClass('active');
+  $('.services header ul li a:contains("Services")').parent().addClass('active');
+  $('.rooms header ul li a:contains("Rooms")').parent().addClass('active');
+  $('.room header ul li a:contains("Rooms")').parent().addClass('active');
+});
